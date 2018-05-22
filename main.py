4e53d4dffcc2cc36375462bfb3d7858a3bf1bdb8
Peter de Vocht <peter@peter.co.nz>
2018-05-23 10:27:50 +1200

    added intent

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from trainer.train import train
 
 
 # create a graph from a series of files
-# create_graph(['data/news.en-00001-of-00100.parsed'], 'data/graph_01.txt')
+# create_graph(['graph_data/news.en-00001-of-00100.parsed'], 'graph_data/graph_01.txt')
 
 # train a neural network for intent detection
 train('intent_training.txt', 10)
